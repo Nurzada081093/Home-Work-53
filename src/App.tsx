e9cd67b7ea80010d235b4a5a7fcc47b49b10e003
Zhumakadyrova Nurzada <zhumakadyrova.nurzat081093@gmail.com>
2024-09-19 11:53:08 +0600

    Создание стейта и вывод компонента Task через цикл в файле App.tsx. Так жебыло добавлено восклицательный знак при выводе задачи.

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,42 @@
 import './App.css';
 import Task from './Components/Task/Task';
 import AddTaskForm from './Components/AddTaskForm/AddTaskForm';
-import React from 'react';
+import React, {useState} from 'react';
+
+interface ITask {
+    task: string;
+    id: string;
+}
 
 const App = () => {
+
+    const [tasks, setTasks] = useState<ITask[]>([
+        {id: '1', task: 'Finish my course'},
+        {id: '2', task: 'Find a job'},
+        {id: '3', task: 'Get a good salary'},
+    ]);
+
 
     const getTasks = (event: React.ChangeEvent<HTMLInputElement>) => {
         console.log(event.target.value);
     };
 
     const addTasks = () => {
-
+        console.log('Click me!');
     };
 
-    const removeTaskFromBlock = () => {
-        console.log('click me!');
+    const removeTaskFromBlock = (id:string) => {
+        const copyTasks = tasks.filter((task) => task.id !== id);
+        setTasks(copyTasks);
     };
+
+    const displayTasks = tasks.map((displayTask) => (
+        <Task
+            key={displayTask.id}
+            task={displayTask.task}
+            removeTask={() => removeTaskFromBlock(displayTask.id)}
+        />
+    ));
 
 
     return (
@@ -23,9 +44,7 @@ const App = () => {
             <h1 className="title">Plans of your future!</h1>
             <AddTaskForm getTask={getTasks} addTask={addTasks}/>
             <div className="tasks-block">
-                <Task task="Сделать уроки по скорее!!!" removeTask={removeTaskFromBlock}/>
-                <Task task="Сделать уроки по скорее!!!" removeTask={removeTaskFromBlock}/>
-                <Task task="Сделать уроки по скорее!!!" removeTask={removeTaskFromBlock}/>
+                {displayTasks}
             </div>
         </div>
 
--- src/Components/Task/Task.tsx
+++ src/Components/Task/Task.tsx
@@ -2,16 +2,19 @@ import './Task.css';
 import React from 'react';
 
 interface ITask {
-    task:string;
+    task: string;
     removeTask: React.MouseEventHandler;
 }
+
 
 const Task: React.FC<ITask> = ({task, removeTask}) => {
     return (
         <div className="tasks">
-            <p className="task-info">{task}</p>
+            <p className="task-info">{task}!</p>
             <div className="button-delete">
-                <button type="button" onClick={removeTask} className="delete"><img className="delete-icon" src="https://img.icons8.com/carbon-copy/50/filled-trash.png" alt="filled-trash"/></button>
+                <button type="button" onClick={removeTask} className="delete"><img className="delete-icon"
+                                                                                   src="https://img.icons8.com/carbon-copy/50/filled-trash.png"
+                                                                                   alt="filled-trash"/></button>
             </div>
         </div>
     );
